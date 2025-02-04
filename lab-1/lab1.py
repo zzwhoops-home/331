cdf782f7815917cede08154cff7a0ea4a8c34884
zzwhoops-home <zd@5rock.org>
2025-02-04 00:19:53 -0500

    add loading of map image

--- lab-1/lab1.py
+++ lab-1/lab1.py
@@ -8,30 +8,46 @@ PX_WIDTH = 10.29
 PX_HEIGHT = 7.55
 
 # TERRAIN SPEEDS
-class Terrain:
-    OPEN = 5
-    MEADOW = 0.5
-    EASY_FOREST = 4
-    JOG_FOREST = 3
-    WALK_FOREST = 2
-    IMPASSIBLE = 0.01
-    SWAMP = 0.25
-    PAVED_ROAD = 6
-    FOOTPATH = 5
-
+SPEEDS = {
+    "OPEN": 5,
+    "MEADOW": 0.5,
+    "EASY_FOREST": 4,
+    "JOG_FOREST": 3,
+    "WALK_FOREST": 2,
+    "IMPASSIBLE": 0.01,
+    "SWAMP": 0.25,
+    "PAVED_ROAD": 6,
+    "FOOTPATH": 5,
+    "OUT_OF_BOUNDS": 0
+}
 # COLOR CONSTANTS
-class Color:
-    OPEN_LAND = "#F89412"
-    ROUGH_MEADOW = "#FFC000"
-    EASY_MOVEMENT_FOREST = "#FFFFFF"
-    SLOW_RUN_FOREST = "#02D03C"
-    WALK_FOREST = "#028828"
-    IMPASSIBLE_VEGETATION = "#054918"
-    LAKE_SWAMP_MARSH = "#0000FF"
-    PAVED_ROAD = "#473303"
-    FOOTPATH = "#000000"
-    OUT_OF_BOUNDS = "#CD0065"
-    OPTIMAL_PATH = "#A146DD" 
+COLORS = {
+    "OPEN": (248, 148, 18),
+    "MEADOW": (255, 192, 0),
+    "EASY_FOREST": (255, 255, 255),
+    "JOG_FOREST": (2, 208, 60),
+    "WALK_FOREST": (2, 136, 40),
+    "IMPASSIBLE": (5, 73, 24),
+    "SWAMP": (0, 0, 255),
+    "PAVED_ROAD": (71, 51, 3),
+    "FOOTPATH": (0, 0, 0),
+    "OUT_OF_BOUNDS": (205, 0, 101),
+    "OPTIMAL_PATH": (161, 70, 221)
+}
+
+# REFERENCE FOR O(1) SEARCH
+COLOR_REF = {
+    (248, 148, 18): "OPEN",
+    (255, 192, 0): "MEADOW",
+    (255, 255, 255): "EASY_FOREST",
+    (2, 208, 60): "JOG_FOREST",
+    (2, 136, 40): "WALK_FOREST",
+    (5, 73, 24): "IMPASSIBLE",
+    (0, 0, 255): "SWAMP",
+    (71, 51, 3): "PAVED_ROAD",
+    (0, 0, 0): "FOOTPATH",
+    (205, 0, 101): "OUT_OF_BOUNDS"
+}
 
 # get command line args
 args = sys.argv
@@ -44,7 +60,35 @@ def process_elev():
     with open(elev_path, "r") as f:
         arr = f.readlines()
         result = [line.split()[:-5] for line in arr]
-        print(result[0], len(result[0]))
+        return result
+
+def process_path():
+    with open(path_path, "r") as f:
+        arr = f.readlines()
+        result = [[int(num) for num in line.split()] for line in arr]
+        return result
+
+def process_img():
+    img = Image.open(img_path, "r")
+    pixels = img.load()
+    width, height = img.size
+    
+    map = [[0 for i in range(width)] for i in range(height)]
+
+    for x in range(width):
+        for y in range(height):
+            pixel = pixels[x, y]
+            colors = (pixel[0], pixel[1], pixel[2])
+            if (colors in COLOR_REF):
+                map[y][x] = SPEEDS[COLOR_REF[colors]]
+
+    return map
+
 
 if __name__ == "__main__":
-    process_elev()
+    # process elevation file
+    elev_map = process_elev()
+    path_points = process_path()
+    # print(path_points)
+    map = process_img()
+    print(map)
