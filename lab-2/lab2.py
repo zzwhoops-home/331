8acfe11c742f74735d2ba3589b64e0581a1e9b69
zzwhoops-home <zd@5rock.org>
2025-02-12 12:26:46 -0500

    read file lab2

--- lab-2/lab2.py
+++ lab-2/lab2.py
@@ -3,3 +3,11 @@ import sys
 # get command line args
 args = sys.argv
 KB_PATH = args[1]
+
+def process_file():
+    with open(KB_PATH, "r") as f:
+        lines = [line.strip() for line in f.readlines()]
+        print(lines)
+
+if __name__ == "__main__":
+    process_file()
