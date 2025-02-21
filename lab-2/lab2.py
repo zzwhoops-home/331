19587cdda3981e6413a05bbb9301854e07f81c24
zzwhoops-home <zd@5rock.org>
2025-02-21 17:38:04 -0500

    add file reading and example constants only cnf

--- lab-2/lab2.py
+++ lab-2/lab2.py
@@ -7,7 +7,37 @@ KB_PATH = args[1]
 def process_file():
     with open(KB_PATH, "r") as f:
         lines = [line.strip() for line in f.readlines()]
-        print(lines)
+        preds = lines[0].split(": ")
+        vars = lines[1].split(": ")
+        consts = lines[2].split(": ")
+        funcs = lines[3].split(": ")
+        clauses = lines[5:]
+
+        if (len(preds) > 1):
+            preds = preds[1].split(" ")
+        else:
+            preds = []
+
+        if (len(vars) > 1):
+            vars = vars[1].split(" ")
+        else:
+            vars = []
+
+        if (len(consts) > 1):
+            consts = consts[1].split(" ")
+        else:
+            consts = []
+            
+        if (len(funcs) > 1):
+            funcs = funcs[1].split(" ")
+        else:
+            funcs = []
+
+        print(preds)
+        print(vars)
+        print(consts)
+        print(funcs)
+        print(clauses)
 
 if __name__ == "__main__":
     process_file()
