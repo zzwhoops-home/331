b7ea0fe415c728d9b88b34faae993a81351b0f4f
zzwhoops-home <zd@5rock.org>
2025-03-30 18:32:54 -0400

    add temporary file parsing

--- hw-3/hw3.py
+++ hw-3/hw3.py
@@ -4,5 +4,19 @@ import sys
 args = sys.argv
 data = args[1]
 
+attributes = {}
+
 with open("data.dat", "r") as f:
-    print(f.readlines())
+    lines = f.readlines()
+
+    for line in lines:
+        line_list = line.strip().split(" ")
+        attribs = line_list[:-1]
+        result = line_list[-1]
+
+        if (result not in attributes):
+            attributes[result] = []
+        attributes[result].append(attribs)
+
+if __name__ == "__main__":
+    print(attributes)
