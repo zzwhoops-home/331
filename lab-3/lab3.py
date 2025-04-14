8bb2f3141c7bd4e480786b49687df59c8bdabe85
zzwhoops-home <zd@5rock.org>
2025-04-13 23:46:12 -0400

    add ada boost algorithm from pseudocode

--- lab-3/lab3.py
+++ lab-3/lab3.py
@@ -7,6 +7,7 @@ import pickle
 
 # FOR DEBUGGING USE:
 # lab-3/train.dat lab-3/features.txt lab-3/models/best.model dt
+# lab-3/train.dat lab-3/features.txt lab-3/models/best.model ada
 # get command line args
 args = sys.argv
 run_type = args[1] # either 'train' to train on dt or ada, or 'predict' to predict on examples
@@ -22,12 +23,16 @@ elif (run_type == "predict"):
     features_path = args[3] # path to set of features
     hypo_path = args[4]
 
+# global variables
 examples = []
+example_weights = None
 attributes_names = None
 attributes_key = None
 unique_attrib_vals = {}
-ex_count = {}
 total_count = 0
+ex_count = {}
+hypotheses = None
+hypotheses_weights = None
 
 POS_EX = "en"
 NEG_EX = "nl"
@@ -35,8 +40,7 @@ NEG_EX = "nl"
 def read_features_file():
     """Helper function to read in file and load attributes
     """
-    global attributes_names
-    global attributes_key
+    global attributes_names, attributes_key
 
     # get attributes
     with open(features_path, "r", encoding="utf-8") as f:
@@ -54,9 +58,7 @@ def read_features_file():
 def read_train_examples_file():
     """Helper function to read in file and load examples
     """
-    global examples
-    global total_count
-    global attributes_names
+    global examples, total_count, attributes_names
 
     with open(examples_path, "r", encoding="utf-8") as f:
         lines = f.readlines()
@@ -88,27 +90,36 @@ def read_train_examples_file():
         # get total count
         total_count = sum([i for i in ex_count.values()])
 
-def read_test_examples_file(dt):
-    """Helper function to read in file and load test examples
+def read_test_examples_file(type, dt=None, hypotheses=None, hypotheses_weights=None):
+    """Helper function to read in file, load test examples, and print predictions for normal decision tree
     """
-    global examples
-    global total_count
-    global attributes_names
-
     with open(examples_path, "r", encoding="utf-8") as f:
         lines = f.readlines()
 
-        nl = 0
-        en = 0
-        for line in lines:
-            classification = process_and_classify(dt, line)
-            print(classification)
-            if (classification == "nl"):
-                nl += 1
-            elif (classification == "en"):
-                en += 1
+        if (type == "dt"):
+            nl = 0
+            en = 0
+            for line in lines:
+                classification = process_and_classify(dt, line)
+                print(classification)
+                if (classification == "nl"):
+                    nl += 1
+                elif (classification == "en"):
+                    en += 1
 
-        print(f"\nEN: {en}, NL: {nl}")
+            # print(f"\nEN: {en}, NL: {nl}")
+        elif (type == "ada"):
+            nl = 0
+            en = 0
+            for line in lines:
+                classification = process_and_classify_ada(hypotheses, hypotheses_weights, line)
+                print(classification)
+                if (classification == "nl"):
+                    nl += 1
+                elif (classification == "en"):
+                    en += 1
+
+            # print(f"\nEN: {en}, NL: {nl}")
 
 def build_dt(exs, attribs, parent_exs=None, depth=0, max_depth=None):
     """Builds a decision tree from a list of examples, attributes, and parent examples
@@ -244,14 +255,14 @@ def importance_counter(exs):
     pos = 0
     neg = 0
 
-    for ex in exs:
-        classification = list(ex.keys())[0]
+    for i in range(len(exs)):
+        classification = list(exs[i].keys())[0]
         if (classification == POS_EX):
-            pos += 1
+            pos += example_weights[i]
         elif (classification == NEG_EX):
-            neg += 1
+            neg += example_weights[i]
         else:
-            print("Make sure you change the pos/neg examples!!")
+            print(f"Make sure you change the pos/neg examples for training on differently-labeled items! Current: pos: {POS_EX}, neg: {NEG_EX}")
     
     # returns (pos, neg) tuple for access
     return (pos, neg)
@@ -288,14 +299,19 @@ def majority(exs: list):
     Returns:
         type of elements in exs: The output value in the examples provided which appears the most 
     """
+    global example_weights
+
     # count total of each item
     count = {}
-    for ex in exs:
-        classification = list(ex.keys())[0]
+    for i in range(len(exs)):
+        # get classification
+        classification = list(exs[i].keys())[0]
+
+        # add example weight
         if (classification not in count):
-            count[classification] = 1
+            count[classification] = example_weights[i]
         else:
-            count[classification] += 1
+            count[classification] += example_weights[i]
 
     # keep track of majority count and classification 
     majority_count = 0
@@ -376,7 +392,7 @@ def get_unique_attrib_vals(exs: list):
         values = set([list(ex.values())[0][attributes_key[attrib]] for ex in exs])
         unique_attrib_vals[attrib] = values
 
-def process_and_classify(node: Node, example: list[bool]):
+def process_and_classify(node: Node, example: str):
     """Processes data and calls recursive function to classify the set of data
 
     Args:
@@ -393,6 +409,29 @@ def process_and_classify(node: Node, example: list[bool]):
 
     # call recursive classify function and return result
     return classify(node, attribs)
+
+def process_and_classify_ada(hypotheses: list[Node], hypotheses_weights: list[float], example: str):
+    votes = dict()
+
+    # get all classifications
+    for k in range(len(hypotheses)):
+        classification = process_and_classify(hypotheses[k], example)
+
+        # add weighted vote to votes
+        if (classification not in votes):
+            votes[classification] = hypotheses_weights[k]
+        else:
+            votes[classification] += hypotheses_weights[k]
+
+    # get highest weighted vote sum
+    majority_vote = float("-inf")
+    majority_class = None
+    for key, value in votes.items():
+        if (value > majority_vote):
+            majority_class = key
+            majority_vote = value
+    
+    return majority_class
 
 def classify(node: Node, attribs: list[bool]):
     """Does the actual classification steps recursively
@@ -419,6 +458,70 @@ def classify(node: Node, attribs: list[bool]):
     # fallback, throw an error
     raise Exception("Warning: could not classify example!")
 
+def ada(exs, ensemble_depth=2):
+    global example_weights, hypotheses, hypotheses_weights
+    total_h = len(hypotheses)
+
+    # smallest possible # to prevent division-by-zero
+    epsilon = sys.float_info.epsilon
+    # track classification
+    classification = None
+
+    for k in range(total_h):
+        # build decision tree with given depth
+        hypotheses[k] = build_dt(exs=examples, attribs=attributes_names, max_depth=ensemble_depth)
+        # initial error is 0
+        error = 0
+
+        # compute total error for hypotheses[k]
+        for j in range(len(exs)):
+            # if the hypothesis predicts wrongly, add to error
+            ex_answer = list(exs[j].keys())[0]
+            ex_vals = list(exs[j].values())[0]
+
+            classification = classify(hypotheses[k], ex_vals)
+            # we skip "process" and classify because our train set has already been processed
+            if (classification != ex_answer):
+                error += example_weights[j]
+            
+        # break from loop if error > 1/2 (error too high for hypothesis)
+        if (error > 1 / 2):
+            break
+        # error is min(err, 1 - epsilon)
+        error = min(error, 1 - epsilon)
+
+        # give more weight for examples h[k] got wrong
+        for j in range(len(exs)):
+            ex_answer = list(exs[j].keys())[0]
+            ex_vals = list(exs[j].values())[0]
+
+            classification = classify(hypotheses[k], ex_vals)
+            if (classification == ex_answer):
+                # w[j] = w[j] * (error / (1 - error))
+                example_weights[j] = example_weights[j] * (error / (1 - error))
+        
+        # normalize weights to sum to 1
+        normalize_weights()
+
+        # give more weight to accurate h[k]: hypothesis_weights[k] = (1/2) * log((1 - error) / error)
+        hypotheses_weights[k] = (1 / 2) * math.log((1 - error) / error)
+
+        # return weighted majority of hypothesis predictions
+        return weighted_majority()
+        
+def weighted_majority():
+    global hypotheses, hypotheses_weights
+
+
+def normalize_weights():
+    """Normalizes the weights (global vars) of the examples when AdaBoosting
+    """
+    global example_weights
+    total = sum(example_weights)
+
+    new_weights = [wt / total for wt in example_weights]
+    example_weights = new_weights
+
 if __name__ == "__main__":
     # read in attributes from features file
     read_features_file()
@@ -431,22 +534,62 @@ if __name__ == "__main__":
         # get unique values for each attribute (should be just POS and NEG example defined at the beginning of the file)
         get_unique_attrib_vals(exs=examples)
 
-        max_depth = 6
-        dt = build_dt(exs=examples, attribs=attributes_names, max_depth=max_depth)
+        # initialize all weights to 1 / total examples (will remain this value if normal dt, otherwise they will be updated with adaboost)
+        if (example_weights == None):
+            example_weights = [1 / total_count for i in range(total_count)]
+        
+        MAX_DEPTH = 6
+        dt = build_dt(exs=examples, attribs=attributes_names, max_depth=MAX_DEPTH)
 
+        if (learning_type == "ada"):
+            ADA_TREES = 4
+            
+            # populate hypotheses if needed
+            if (not hypotheses):
+                hypotheses = [None for i in range(ADA_TREES)]
+                hypotheses_weights = [None for i in range(ADA_TREES)]
+
+            ada(exs=examples)
         # dt_graph = visualize_dt(dt)
         # dt_graph.render(f'{max_depth + 1}-layer Decision Tree', view=False)
 
         # serialize for later use
+        to_save = None
+
+        if (learning_type == "dt"):
+            to_save = {
+                "type": "dt",
+                "dt": dt
+            }
+        elif (learning_type == "ada"):
+            to_save = {
+                "type": "ada",
+                "hypotheses": hypotheses,
+                "hypotheses_weights": hypotheses_weights
+            }
+
         with open(out_path, "wb") as f:
-            pickle.dump(dt, f)
+            pickle.dump(to_save, f)
     elif (run_type == "predict"):
         # load serialized model
         with open(hypo_path, "rb") as f:
-            dt = pickle.load(f)
+            data = pickle.load(f)
+            dt_type = data["type"]
 
-        # read in examples
-        read_test_examples_file(dt)
+        # differentiate by learning type
+        if (dt_type == "dt"):
+            # get dt node
+            dt = data["dt"]
+
+            # print out classifications with normal dt
+            read_test_examples_file(type=dt_type, dt=dt)
+        elif (dt_type == "ada"):
+            # get hypotheses and their weights
+            hypotheses = data["hypotheses"]
+            hypotheses_weights = data["hypotheses_weights"]
+
+            # print out classifications with adaboosted ensemble model
+            read_test_examples_file(type=dt_type, hypotheses=hypotheses, hypotheses_weights=hypotheses_weights)
 
         # string = "De kat liep rustig door de tuin terwijl de vogels zongen in de hoge bomen."
         # string = "This is a string that I'm using to test this function. Does it work? idk."
